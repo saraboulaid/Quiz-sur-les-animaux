0c4f0b259990e7cf71ce5cab16148bcb8afce8e3
imane-benattou <imane.benattou@etu.uae.ac.ma>
2025-01-15 11:46:49 +0100

    add javadoc documentation

---
+++ Animal_Quiz/documentation/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.example.animal_quiz"},{"l":"com.example.animal_quiz.models"},{"l":"com.example.animal_quiz.utils"}];updateSearchResults();
